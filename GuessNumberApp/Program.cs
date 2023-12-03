ce8862fd3126876be3b2da8a9eeee5f923897906
smhussain5 <139831097+smhussain5@users.noreply.github.com>
2023-12-02 20:48:42 -0600

    First commit
    Signed-off-by: smhussain5 <139831097+smhussain5@users.noreply.github.com>

---
+++ GuessNumberApp/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace GuessNumberApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            bool playAgain = true;
+            int numGuesses;
+            int userNum;
+            String userReplay;
+
+            while (playAgain)
+            {
+                numGuesses = 0;
+                userNum = 0;
+                int randomInt = random.Next(1, 101);
+
+                Console.WriteLine("I'm thinking of a number from 1-100...");
+
+                while (userNum != randomInt)
+                {
+                    userNum = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine($"Your answer: {userNum}");
+
+                    if (userNum > randomInt)
+                    {
+                        Console.WriteLine($"Sorry! {userNum} is too high!");
+                    }
+                    else if (userNum < randomInt)
+                    {
+                        Console.WriteLine($"Sorry! {userNum} is too low!");
+                    }
+                    numGuesses++;
+                }
+                Console.WriteLine("CONGRATULATIONS! YOU GOT IT!");
+                Console.WriteLine($"ANSWER: {randomInt}");
+                Console.WriteLine($"It only took you {numGuesses} tries!");
+                Console.WriteLine("Play again? (Y/N)");
+                userReplay = Console.ReadLine();
+                userReplay = userReplay.ToUpper();
+                if (userReplay == "Y")
+                {
+                    playAgain = true;
+                } else if (userReplay == "N")
+                {
+                    playAgain = false;
+                }
+            }
+
+            Console.WriteLine("Thanks for playing! See you next time!");
+
+            Console.ReadKey();
+        }
+    }
+}
